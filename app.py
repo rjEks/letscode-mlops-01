83ffef71f96eba20e7630590aaaac28ec3af32ff
rjEks <rjekstein@gmail.com>
2021-07-26 17:56:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,16 @@ import numpy as np
 
 def handler(event, context):
 
-
+    #Leitura do Payload
     sepal_length = float(event.get('Iris')['sepal_length'])
     sepal_width = float(event.get('Iris')['sepal_width'])
     petal_length = float(event.get('Iris')['petal_length'])
     petal_width = float(event.get('Iris')['petal_width'])
 
+    #Reshape Dataset
     row_arti = np.array([sepal_length,sepal_width,petal_length,petal_width]).reshape(1,4)
 
+    #Load do Modelo
     modelo = joblib.load("modelo.joblib")
     resposta = int(modelo.predict(row_arti)[0])
 
